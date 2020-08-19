31984be0aac4744b63fbf24f63c990116d1f8e27
Dalal7 <46856168+Dalal7@users.noreply.github.com>
2020-08-19 16:54:56 +0300

    Add files via upload

---
+++ word_pdf_convertor/Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="word_pdf_convertor.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="StyleSheet1.css" rel="stylesheet" />
+    <style type="text/css">
+        .auto-style1 {
+            height: 172px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+           
+            <h1>PDF Convertor <span> convert PDF files into word documents</span></h1>
+
+<div class="auto-style1">
+    <h3>&nbsp;</h3>
+    <h3>&nbsp;</h3>
+    <h3>&nbsp;</h3>
+    <h3>Choose a file to convert:</h3>
+  <asp:FileUpload ID="file" runat="server" Height="24px" Width="205px" />
+    <br />
+    <asp:Label ID="Result" runat="server"></asp:Label>
+    <br />
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="convert" Width="121px" />
+</div>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ word_pdf_convertor/Default.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SautinSoft;
+
+namespace word_pdf_convertor
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (file.HasFile)
+            {
+                try
+                {
+
+                    if (file.PostedFile.FileName.Length == 0 || file.FileBytes.Length == 0)
+                    {
+                        Result.Text = "Please select PDF file at first!";
+                        return;
+                    }
+                    byte[] rtf = null;
+
+                    SautinSoft.PdfFocus f = new SautinSoft.PdfFocus();
+                    f.OpenPdf(file.FileBytes);
+
+                    if (f.PageCount > 0)
+                    {
+                        //Let's whole PDF document to Word (RTF)
+                        f.WordOptions.Format = SautinSoft.PdfFocus.CWordOptions.eWordDocument.Rtf;
+
+                        // You may also set an output format to Docx.
+                        //f.WordOptions.Format = SautinSoft.PdfFocus.CWordOptions.eWordDocument.Docx;
+                        rtf = f.ToWord();
+                    }
+
+                    //show Word/rtf
+                    if (rtf != null)
+                    {
+                        ShowResult(rtf, "Result.rtf", "application/msword");
+                    }
+                    else
+                    {
+                        Result.Text = "Converting failed!";
+                    }
+                
+   
+
+            }
+                catch (Exception ex)
+                {
+                    Response.Write("Error: " + ex.Message);
+                }
+            }
+            else
+            {
+                Result.Text = "Please choose a file to upload!";
+            }
+
+        }
+
+        private void ShowResult(byte[] data, string fileName, string contentType)
+        {
+            Response.Buffer = true;
+            Response.Clear();
+            Response.ContentType = contentType;
+            Response.AddHeader("content-disposition", "inline; filename=\"" + fileName + "\"");
+            Response.BinaryWrite(data);
+            Response.Flush();
+            Response.End();
+        }
+    }
+}
